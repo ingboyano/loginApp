7b237bd96bab74563be4d6c932c4a684572e541f
ingboyano <ingboyano03@gmail.com>
2024-07-25 19:40:19 -0400

    3eraParte

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../services/chat.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  chatGroups: any[] = [];
 
-  constructor() {}
+  constructor(private chatService: ChatService) { }
 
+  ngOnInit() {
+    this.chatService.getChatGroups().subscribe((data: any[]) => {
+      this.chatGroups = data;
+    });
+  }
 }
